dc0cafdd7c2bbc466eac994bd715a49437a9b3dd
Billy Shen <shenfangtao@imaodu.com>
2022-10-31 20:44:00 +0800

    update frontend

--- jenkins/scripts/updateFrontend.sh
+++ jenkins/scripts/updateFrontend.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-echo 'build frontend'
-cd sbvadmin-vben
+#服务太拉了，没法云端构建，算了还是把dist放入git管理，然后本地构建后再提交吧
+#echo 'build frontend'
+#cd sbvadmin-vben
 #yarn  安装依赖，只需执行一次
-yarn build
+#yarn build
+echo 'update dist'
+cd adminserver/admin-web/src/main/resources/static
+rm -fr *
+cp ../../../../../
+pwd
+
