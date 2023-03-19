9b2406a8dd0525642aa56aa2695819a5a1f2bd50
Billy Shen <shenfangtao@imaodu.com>
2023-03-19 22:03:23 +0800

    优化i18n

--- adminserver/admin-web/src/main/java/com/sbvadmin/generator/move_files.sh
+++ adminserver/admin-web/src/main/java/com/sbvadmin/generator/move_files.sh
@@ -2,6 +2,7 @@
 echo '*************************开始移动后端文件*************************'
 # 移动自动生成的文件到对应的文件夹，如果已经存在则不覆盖，防止辛苦写的代码被干掉了
 # 这里低代码生产的代码放到了admin-model中的src/main/java/com/${1%.*}/generator/tempFiles
+# ${1%.*}:packageName
 mv -n src/main/java/com/sbvadmin/generator/tempFiles/com/${1%.*}/vue/controller/* ../admin-web/src/main/java/com/${1%.*}/controller
 mv -n src/main/java/com/sbvadmin/generator/tempFiles/com/${1%.*}/model/*.java ../admin-model/src/main/java/com/${1%.*}/model
 mv -n src/main/java/com/sbvadmin/generator/tempFiles/com/${1%.*}/mapper/*.java ../admin-mapper/src/main/java/com/${1%.*}/mapper
@@ -13,8 +14,8 @@ echo '*************************开始移动前端文件*************************
 mv -n src/main/java/com/sbvadmin/generator/tempFiles/com/${1%.*}/vue/api/*.ts ../../../${2%.*}/src/api/${1%.*}
 mv -n src/main/java/com/sbvadmin/generator/tempFiles/com/${1%.*}/vue/model/*.ts ../../../${2%.*}/src/api/${1%.*}/model
 mv -n src/main/java/com/sbvadmin/generator/tempFiles/com/${1%.*}/vue/view/* ../../../${2%.*}/src/views/${1%.*}
-mv -n src/main/java/com/sbvadmin/generator/tempFiles/com/${1%.*}/vue/i18nEN/* ../../../${2%.*}/src/locales/lang/en/routes
-mv -n src/main/java/com/sbvadmin/generator/tempFiles/com/${1%.*}/vue/i18nZH/* ../../../${2%.*}/src/locales/lang/zh-CN/routes
+mv -n src/main/java/com/sbvadmin/generator/tempFiles/com/${1%.*}/vue/i18nEN/* ../../../${2%.*}/src/locales/lang/en/routes/${1%.*}
+mv -n src/main/java/com/sbvadmin/generator/tempFiles/com/${1%.*}/vue/i18nZH/* ../../../${2%.*}/src/locales/lang/zh-CN/routes/${1%.*}
 
 echo '*************************删除自动生成的文件************************'
 rm -fr src/main/java/com/sbvadmin/generator/tempFiles
