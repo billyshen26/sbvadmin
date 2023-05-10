f02ea13529baae09a216e3005fdf765d6ab933e8
Billy Shen <shenfangtao@imaodu.com>
2023-05-10 16:02:35 +0800

    fix:增加修改密码的权限

--- adminserver/admin-web/src/main/resources/db/migration/V20230510154145__add_change_password_permission.sql
+++ adminserver/admin-web/src/main/resources/db/migration/V20230510154145__add_change_password_permission.sql
@@ -2,7 +2,7 @@
 -- 补充修改密码的权限点
 --
 INSERT INTO `sys_permission` (`id`, `pid`, `request_url`, `request_method`, `name`, `title`, `description`, `path`, `component`, `icon`, `show_flag`, `type`, `status`, `order_no`, `created_at`, `updated_at`)
-VALUES (49, 25, 'api/users/changePassword','POST', '修改密码', '','修改密码','','', '','1','2','1','1',now(), now());
+VALUES (49, 25, '/api/users/changePassword','POST', '修改密码', '','修改密码','','', '','1','2','1','1',now(), now());
 
 --
 -- 默认将菜单分配给root
