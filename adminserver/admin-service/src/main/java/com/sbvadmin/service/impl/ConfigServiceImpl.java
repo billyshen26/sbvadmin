2f77d8161037595340d2b340364090d9419ec1c9
Billy Shen <shenfangtao@imaodu.com>
2023-11-30 13:22:45 +0800

    fix配置项修改不起作用2

--- adminserver/admin-service/src/main/java/com/sbvadmin/service/impl/ConfigServiceImpl.java
+++ adminserver/admin-service/src/main/java/com/sbvadmin/service/impl/ConfigServiceImpl.java
@@ -4,6 +4,7 @@ import com.sbvadmin.model.Config;
 import com.sbvadmin.mapper.ConfigMapper;
 import com.sbvadmin.service.IConfigService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +17,16 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ConfigServiceImpl extends ServiceImpl<ConfigMapper, Config> implements IConfigService {
-
+    /**
+     * Notes:  复写更新，主要用于配置项改动后，更新缓存， 目前会删除所有config下的缓存，后期再优化成只更新当前修改的配置项 TODO
+     * @param: [entity]
+     * @return: boolean
+     * Author: 涛声依旧 likeboat@163.com
+     * Time: 2023/11/29 15:29
+     **/
+    @Override
+    @CacheEvict(value="config",  allEntries = true)
+    public boolean updateById(Config entity) {
+        return IConfigService.super.updateById(entity);
+    }
 }
