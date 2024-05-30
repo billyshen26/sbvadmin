041f6ecf6d62552225008599920aaf08374f1822
Billy Shen <shenfangtao@imaodu.com>
2024-05-30 15:05:24 +0800

    java序列化漏洞

---
+++ mailserver/src/main/java/com/sbvadmin/mailserver/config/RabbitMQConfig.java
@@ -0,0 +1,20 @@
+package com.sbvadmin.mailserver.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Notes: https://blog.csdn.net/m0_64694079/article/details/134935219  java序列化漏洞问题
+ * Author: 涛声依旧 likeboat@163.com
+ * Time: 2024/5/30 11:32
+ */
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
